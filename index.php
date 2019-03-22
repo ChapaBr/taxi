1504fa66fd27c77d5e26f07ae6d300f054cd6510
ChapaBr <gui_2512@hotmail.com>
2019-03-22 10:11:13 -0300

    Origin Master

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require_once('layouts/header.php')
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Taxi</title>
+</head>
+<body>
+
+</body>
+</html>
+<?php
+require_once('layouts/footer.php')
+?>
---
+++ layouts/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fi=no">
+	<title>Taxi</title>
+
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="libs/bootstrap-4.3.1-dist/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="libs/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+	
+</head>
+<body>
